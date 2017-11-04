ad765c7ecb2577eb9f69a7612201b67411c767b5
Ryan Hanover <rhan913@gmail.com>
2017-11-04 18:25:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var katzDeliLine = [];
 
 
 function takeANumber(katzDeliLine, newCustomer) {
-  
+
   if (newCustomer === undefined){
     console.log("No number was taken");
   }
   else {
   katzDeliLine.push(newCustomer);
-  
+
   console.log(`Welcome , ${newCustomer}.You are number ${katzDeliLine.length } in line !  `);
 
   }
@@ -16,7 +16,7 @@ function takeANumber(katzDeliLine, newCustomer) {
 
 
 
-                    
+
 function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0) {
     console.log("There is nobody waiting to be served!");
