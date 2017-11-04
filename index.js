70e9d3268dd0bb151601a47a0a6bf59f0a67e19b
Ryan Hanover <rhan913@gmail.com>
2017-11-04 18:51:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,26 +2,27 @@ var katzDeliLine = [];
 
 
 function takeANumber(katzDeliLine, newCustomer) {
-
-
+  
+ 
   katzDeliLine.push(newCustomer);
+  
+  return `Welcome , ${newCustomer}. You are number ${katzDeliLine.length } in line.`;
 
-  console.log(`Welcome , ${newCustomer}. You are number ${katzDeliLine.length } in line.`);
-
-
+  
 }
 
 
 
-
+                   
 function nowServing(katzDeliLine){
   if (katzDeliLine.length < 1) {
-    console.log("There is nobody waiting to be served!");
+    return "There is nobody waiting to be served!";
   }
   else {
-   console.log(`Now serving ${katzDeliLine[0]} !`);
+   return `Currently serving ${katzDeliLine[0]}.`,
+   
    katzDeliLine.splice(0,1);
-
+   
    ///another way to remove ?///
   }
 }
@@ -31,9 +32,9 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   if (katzDeliLine.length < 1) {
-    console.log("The line is currently empty.");
+    return "The line is currently empty.";
   }
   else {
-    console.log(`The line is currently: ${katzDeliLine}`);
+    return `The line is currently: ${katzDeliLine}`;
   }
 }
