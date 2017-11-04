ed39efe160daa3a727312c0b92bd3d01d9a24759
Ryan Hanover <rhan913@gmail.com>
2017-11-04 18:51:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ var katzDeliLine = [];
 
 
 function takeANumber(katzDeliLine, newCustomer) {
-  
- 
+
+
   katzDeliLine.push(newCustomer);
-  
+
   return `Welcome , ${newCustomer}. You are number ${katzDeliLine.length } in line.`;
 
-  
+
 }
 
 
 
-                   
+
 function nowServing(katzDeliLine){
   if (katzDeliLine.length < 1) {
     return "There is nobody waiting to be served!";
   }
   else {
    return `Currently serving ${katzDeliLine[0]}.`,
-   
+
    katzDeliLine.splice(0,1);
-   
+
    ///another way to remove ?///
   }
 }
