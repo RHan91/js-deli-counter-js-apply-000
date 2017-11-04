3c92b59665e7419125a8b1bc76a0d8b80013d45a
Ryan Hanover <rhan913@gmail.com>
2017-11-04 18:44:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ var katzDeliLine = [];
 
 
 function takeANumber(katzDeliLine, newCustomer) {
-
-
+  
+ 
   katzDeliLine.push(newCustomer);
+  
+  console.log(`Welcome , ${newCustomer}. You are number ${katzDeliLine.length } in line.`);
 
-  console.log(`Welcome , ${newCustomer}.You are number ${katzDeliLine.length } in line !  `);
-
-
+  
 }
 
 
 
-
+                   
 function nowServing(katzDeliLine){
   if (katzDeliLine.length < 1) {
     console.log("There is nobody waiting to be served!");
   }
   else {
    console.log(`Now serving ${katzDeliLine[0]} !`);
+   katzDeliLine.splice(0,1);
+   
+   ///another way to remove ?///
   }
 }
 
