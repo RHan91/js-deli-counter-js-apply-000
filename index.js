544c1d046cb9b62cbb2bf4c1b11dacd8ff925843
Ryan Hanover <rhan913@gmail.com>
2017-11-04 18:52:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, newCustomer) {
 
   katzDeliLine.push(newCustomer);
 
-  return `Welcome  , ${newCustomer}. You are number ${katzDeliLine.length } in line.`;
+  return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length } in line.`;
 
 
 }
